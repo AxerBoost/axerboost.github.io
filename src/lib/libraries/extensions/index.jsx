885ad557479d3105166aaf51eccb93913dadac86
Cooldevv1 <iamcooldev@proton.me>
2025-05-18 19:13:02 +0100

    Update index.jsx

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -232,7 +232,7 @@ const menuItems = [
     },
     {
         name: 'AxerAI',
-        extensionId: 'https://static.staticsave.com/hithere/extension.js',
+        extensionId: 'notapplicable',
         iconURL: axerAIInsetIconURL,
         insetIconURL: axerAIIconURL,
         customInsetColor: '#76244D',
